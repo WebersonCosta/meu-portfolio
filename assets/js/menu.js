8e1e6c2b92ad1591d74a3cfacfdecc68a7beed9f
Weberson Costa <weberson.dev0@gmail.com>
2024-02-10 13:49:58 -0300

    Adicionado efeito de seção ativa

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,4 @@
+window.addEventListener('scroll', onScroll)
 let btnMenu = document.getElementById('btn-open-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
@@ -13,3 +14,41 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('open-menu')
 })
+
+onScroll()
+function onScroll(){
+    activateMenuAtCurrentSection(topofsite)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(skills)
+    activateMenuAtCurrentSection(portfolio)
+}
+
+function activateMenuAtCurrentSection(section){
+
+    const targetLine = scrollY + innerHeight / 2
+    
+    // O topo da seção
+    const sectionTop = section.offsetTop
+    // A altura da seção
+    const sectionHeight = section.offsetHeight
+    // O topo da seção chegou ou ultrapassou a linha alvo
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop 
+    // Verifica onde a seção termina
+    const sectionEndsAt = sectionTop + sectionHeight
+    // Verifica se a base está passando da linha alvo
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+
+    // Limites da seção
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu-desktop a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if(sectionBoundaries){
+        menuElement.classList.add('active')
+    }
+    
+}
